da19a72e60b6786605ee88dd14ad591db2074aca
cedric <cedric@studio-sept.be>
2015-07-19 23:42:24 +0200

    Add TourismPortal interests

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   mount Upmin::Engine => '/admin'
   root to: 'visitors#index'
   devise_for :users
+  resources :interests, only: [:index, :show]
+
+  resources :categories, only: :index do
+    resources :interests, only: :index
+  end
+
 end
